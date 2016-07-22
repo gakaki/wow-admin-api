424275a42477b784ca21ef6506376f0556919c34
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-07-22 23:25:27 +0800

    生成产品

--- src/main/java/com/wow/adminapi/controller/ProductController.java
+++ src/main/java/com/wow/adminapi/controller/ProductController.java
@@ -1,64 +1,171 @@
 package com.wow.adminapi.controller;
 
+import com.wow.common.request.ApiRequest;
 import com.wow.common.response.ApiResponse;
+import com.wow.common.response.CommonResponse;
 import com.wow.common.util.ErrorCodeUtil;
-import com.wow.product.model.Brand;
-import com.wow.product.model.Product;
+import com.wow.common.util.JsonUtil;
+import com.wow.common.util.StringUtil;
+import com.wow.common.util.ValidatorUtil;
+import com.wow.price.service.PriceService;
 import com.wow.product.service.BrandService;
+import com.wow.product.service.DesignerService;
+import com.wow.product.service.ProductSerialService;
 import com.wow.product.service.ProductService;
+import com.wow.product.vo.request.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/products")
-public class ProductController {
+@CrossOrigin(maxAge = 3600)
+public class ProductController extends BaseController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     @Autowired
-    private BrandService brandService;
-    @Autowired
     private ProductService productService;
 
-    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
-    public ApiResponse getProduct(@PathVariable Integer productId) {
-        ApiResponse apiResponse = new ApiResponse();
-        Product product = productService.getProductById(productId);
-        if (product != null) {
-            apiResponse.setResCode("0");
-            apiResponse.setResMsg("Success");
-            apiResponse.setData(product);
+    @Autowired
+    private ProductSerialService productSerialService;
+
+    @Autowired
+    private PriceService priceService;
+
+    @Autowired
+    private BrandService brandService;
+
+    @Autowired
+    private DesignerService designerService;
+
+    private static final Integer productPrimaryImgCountLimit = 5;
+
+    public static void test(String[] args) {
+        ProductCreateRequest request = new ProductCreateRequest();
+        request.setCategoryId(1);
+        request.setProductName("产品名字");
+        request.setProductModel("型号");
+        request.setSellingPoint("卖点");
+        request.setBrandId(1);
+        request.setOriginCountryId(1);
+        request.setOriginCity("shanghai");
+        request.setStyleId((byte)1);
+        request.setLength((short)100);
+        request.setWidth((short)100);
+        request.setHeight((short)100);
+        request.setApplicablePeople((byte)1);
+        request.setCanCustomized(false);
+        request.setDetailDescription("detail desc");
+
+        List<DesignerVo> designerVoList = new ArrayList<>();
+        DesignerVo designerVo = new DesignerVo();
+        designerVo.setDesignerId(1);
+        designerVo.setPrimary(true);
+        DesignerVo designerVo2 = new DesignerVo();
+        designerVo2.setDesignerId(2);
+        designerVo2.setPrimary(false);
+        designerVoList.add(designerVo);
+        designerVoList.add(designerVo2);
+        request.setDesignerVoList(designerVoList);
+
+        List<Integer> applicableSceneList = new ArrayList<>();
+        applicableSceneList.add(1);
+        applicableSceneList.add(2);
+        request.setApplicableSceneList(applicableSceneList);
+
+        List<Integer> materialList = new ArrayList<>();
+        materialList.add(1);
+        materialList.add(2);
+        request.setMaterialList(materialList);
+
+        List<ProductImgVo> productImageVoList = new ArrayList<>();
+        ProductImgVo productImgVo = new ProductImgVo();
+        productImgVo.setImgUrl("www.baidu.com");
+        productImgVo.setImgDesc("img desc");
+        productImgVo.setPrimary(true);
+        productImgVo.setSortOrder((byte)1);
+        productImageVoList.add(productImgVo);
+
+
+        ProductImgVo productImgVo1 = new ProductImgVo();
+        productImgVo1.setImgUrl("www.baidu.com");
+        productImgVo1.setImgDesc("img desc");
+        productImgVo1.setPrimary(false);
+        productImgVo1.setSortOrder((byte)1);
+        productImageVoList.add(productImgVo1);
+        request.setProductImgVoList(productImageVoList);
+
+        List<ColorSpecVo> colorSpecVoList = new ArrayList<>();
+        ColorSpecVo colorSpecVo = new ColorSpecVo();
+        colorSpecVo.setColorDisplayName("深红色");
+        colorSpecVo.setColorId((byte)1);
+        colorSpecVo.setProductColorImg("www.baidu.com");
+        List<SpecVo> specVoList = new ArrayList<>();
+        SpecVo specVo = new SpecVo();
+        specVo.setArrivalLatency((byte)10);
+        specVo.setCostPrice(BigDecimal.TEN);
+        specVo.setEnabled(true);
+        specVo.setSellPrice(BigDecimal.TEN);
+        specVo.setSpecName("规格名");
+        specVo.setWeight(BigDecimal.TEN);
+        specVoList.add(specVo);
+        colorSpecVo.setSpecVoList(specVoList);
+        colorSpecVoList.add(colorSpecVo);
+        request.setColorSpecVoList(colorSpecVoList);
+        String json = JsonUtil.pojo2Json(request);
+        System.out.println(json);
+
+        ProductCreateRequest productCreateRequest = JsonUtil.fromJSON(json, ProductCreateRequest.class);
+        //判断json格式参数是否有误
+        if (productCreateRequest == null) {
+            System.out.println("failed");
         } else {
-            apiResponse.setResCode("40201");
-            apiResponse.setResMsg(ErrorCodeUtil.getErrorMsg("40201"));
+            System.out.println("success");
+        }
+
+    }
+
+    /**
+     * 创建产品
+     * @param apiRequest
+     * @return
+     */
+    @RequestMapping(value = "/v1/product/create", method = RequestMethod.POST)
+    public ApiResponse createProductInfo(ApiRequest apiRequest) {
+        ApiResponse apiResponse = new ApiResponse();
+        ProductCreateRequest productCreateRequest = JsonUtil.fromJSON(apiRequest.getParamJson(), ProductCreateRequest.class);
+        //判断json格式参数是否有误
+        if (productCreateRequest == null) {
+            setParamJsonParseErrorResponse(apiResponse);
+            return apiResponse;
+        }
+
+        String errorMsg = ValidatorUtil.getError(productCreateRequest);
+        //如果校验错误 则返回
+        if (StringUtil.isNotEmpty(errorMsg)) {
+            setInvalidParameterResponse(apiResponse, errorMsg);
+            return apiResponse;
+        }
+
+        try {
+            CommonResponse commonResponse = productService.createProductInfo(productCreateRequest);
+
+            if (ErrorCodeUtil.isFailedResponse(commonResponse.getResCode())) {
+                setServiceErrorResponse(apiResponse, commonResponse);
+            }
+        } catch (Exception e) {
+            logger.error("创建产品发生错误---" + e);
+            e.printStackTrace();
+            setInternalErrorResponse(apiResponse);
         }
         return apiResponse;
-    }
-
-    @RequestMapping(value = "/brand/{firstLetter}", method = RequestMethod.GET)
-    public List<Brand> getBrandById(@PathVariable String firstLetter) {
-        logger.info("firstLetter is:" + firstLetter);
-        List<Brand> brandList = brandService.getBrandsByFirstLetter(firstLetter);
-        logger.info("brandList:" + brandList);
-        return brandList;
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Product> getProducts() {
-        Product product = new Product();
-        product.setId(1);
-        product.setProductName("testproduct");
-        List<Product> productList = new ArrayList<Product>();
-
-        productList.add(product);
-        return productList;
     }
 }
